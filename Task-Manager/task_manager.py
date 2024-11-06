74fa85039bd1475663f198e046240bce91335f74
GabrFelps <felipemrqs07@gmail.com>
2024-11-05 21:32:14 -0300

    add: task_manager

---
+++ Task-Manager/task_manager.py
@@ -0,0 +1,43 @@
+import psutil
+import subprocess
+import time
+
+
+def iniciar_processo():
+   processo = subprocess.Popen(["firefox"]) 
+   return processo
+
+
+def monitorar_processo(pid_alvo):
+   try:
+       proc = psutil.Process(pid_alvo)
+       print(f"Monitorando processo com PID {pid_alvo}...\n")
+
+
+       while True:
+           if proc.is_running():
+               status = proc.status()
+               cpu_percent = proc.cpu_percent(interval=1)
+               memoria = proc.memory_info().rss / (1024 ** 2)
+
+
+               print(f"Status: {status} | CPU: {cpu_percent}% | Memória: {memoria:.2f} MB")
+           else:
+               print(f"O processo PID {pid_alvo} foi finalizado.")
+               break
+           time.sleep(2)
+   except psutil.NoSuchProcess:
+       print(f"O processo PID {pid_alvo} não foi encontrado.")
+   except psutil.AccessDenied:
+       print(f"Sem permissão para acessar o processo PID {pid_alvo}.")
+
+
+def main():
+   print("Iniciando o processo...\n")
+   processo = iniciar_processo()
+
+
+   monitorar_processo(processo.pid)
+
+
+main()
